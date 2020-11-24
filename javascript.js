e9b3812bec91d86dbb566819c04a50b9f2a361cd
Ashitom Chhetri <ashitom@Ashitoms-MacBook-Pro.local>
2020-11-24 21:21:59 +0545

    completed this nonsense

--- javascript.js
+++ javascript.js
@@ -1,18 +1,18 @@
-document.querySelector('input').addEventListener('click', function() {
-	if (this.checked) {
-		document.documentElement.setAttribute('data-theme', 'dark');
-		localStorage.setItem('theme', 'dark');
-	} else {
-		document.documentElement.setAttribute('data-theme', 'light');
-		localStorage.setItem('theme', 'light');
+(function activateTheme() {
+	var checkbox = document.querySelector('input');
+
+	checkbox.addEventListener('click', function() {
+		if (this.checked) {
+			document.documentElement.setAttribute('data-theme', 'dark');
+			localStorage.setItem('theme', 'dark');
+		} else {
+			document.documentElement.setAttribute('data-theme', 'light');
+			localStorage.setItem('theme', 'light');
+		}
+	});
+
+	var currentTheme = localStorage.getItem('theme');
+	if (currentTheme) {
+		document.documentElement.setAttribute('data-theme', currentTheme);
 	}
-});
-
-var theme = localStorage.getItem('theme');
-
-if (theme === 'dark') {
-	document.querySelector('input').checked = true;
-	documentElement.setAttribute('data-theme', 'dark');
-} else {
-	documentElement.setAttribute('data-theme', 'light');
-}
+})();
